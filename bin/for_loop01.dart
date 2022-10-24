957f16c7d0cc6a017f9813b277c5f0a2a4a1dc68
aror raximmov <axrorraximov108@gmail.com>
2022-10-24 17:36:41 +0500

    01

--- bin/for_loop01.dart
+++ bin/for_loop01.dart
@@ -6,7 +6,13 @@ Args:
 Return: 
   Int: answer
 */
-  return 0;
+  num s = 0;
+  for (int i = 0; i < numbers.length; i += 1) {
+    s += numbers[i];
+  }
+  return s.toInt();
 }
 
-void main() {}
+void main() {
+  print(func([ 4, 5, 6, 3]));
+}
