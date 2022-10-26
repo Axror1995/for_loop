4a57d671ddab984f50d5ea6d863d1cbbeaaebb5c
aror raximmov <axrorraximov108@gmail.com>
2022-10-26 14:49:53 +0500

    08

--- bin/for_loop08.dart
+++ bin/for_loop08.dart
@@ -2,7 +2,7 @@ int func(List<int> numbers, int k, int n) {
     int d = 0;
   for(int i = k; i < n; i = i + 1){
     if(numbers[i] % 2 == 0){
-      d += numbers[i];
+      d += numbers[n];
 
     }
   }
