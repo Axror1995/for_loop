2b7ead4f03e649bfafccade6f6fd1657eebe7ca9
aror raximmov <axrorraximov108@gmail.com>
2022-10-26 14:46:24 +0500

    06

--- bin/for_loop07.dart
+++ bin/for_loop07.dart
@@ -1,4 +1,13 @@
+
+
 int func(List<int> numbers, int k, int n) {
+  int a = 0;
+  for(int i = k; i < n; i = i + 1){
+    if(numbers[i] % 2 == 0){
+      a += numbers[i];
+
+    }
+  }
 /*
    Given the list of numbers, return the sum of the even numbers between k and n in the list
 Args:
@@ -8,7 +17,9 @@ Args:
 Return: 
   Int: answer
 */
-  return 0;
+  return a;
 }
 
-void main() {}
+void main() {
+  print(func([1,2,3,4,6], 0, 4));
+}
